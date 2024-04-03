3294ac2c775735d1b2adb7e66148b0841baa6527
EduardoMRT <eduksoficial@gmail.com>
2024-04-03 12:30:18 -0300

    Listagem de estados

--- drogaria/src/main/java/com/eduardo/v2/drogaria/bean/EstadoBean.java
+++ drogaria/src/main/java/com/eduardo/v2/drogaria/bean/EstadoBean.java
@@ -3,6 +3,7 @@ package com.eduardo.v2.drogaria.bean;
 import com.eduardo.v2.drogaria.domain.Estado;
 import com.eduardo.v2.drogaria.jpa.AdicionaEstado;
 import com.eduardo.v2.drogaria.jpa.BuscaEstado;
+import com.eduardo.v2.drogaria.jpa.ListaEstado;
 import jakarta.annotation.ManagedBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -12,25 +13,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class EstadoBean {
     private final BuscaEstado buscaEstado;
     private  final AdicionaEstado adicionaEstado;
+    private  final ListaEstado listaEstado;
 
     @Autowired
-    public EstadoBean(BuscaEstado buscaEstado, AdicionaEstado adicionaEstado) {
+    public EstadoBean(BuscaEstado buscaEstado, AdicionaEstado adicionaEstado, ListaEstado listaEstado) {
         this.buscaEstado = buscaEstado;
         this.adicionaEstado = adicionaEstado;
+        this.listaEstado = listaEstado;
     }
 
-    @GetMapping("/teste123")
+    @GetMapping("/estadosADM")
     public String getEstado(Model model) {
         Estado estado = buscaEstado.buscar(1L);
         model.addAttribute("estado", estado);
+
+        List<Estado> estadosList = listaEstado.listar();
+        model.addAttribute("estadosList", estadosList);
+
+        Estado estado1 = new Estado();
+        model.addAttribute("estado1", estado1);
+
         return "estados"; //nesse caso seria o mesmo que estado.html por estar na pasta templates
     }
 
-    @PostMapping ("/teste1234")
+    @PostMapping ("/adicionaEstado")
     public String adcEstado(Model model, String nome, String sigla){
         Estado estado = adicionaEstado.adicionar(nome, sigla);
         model.addAttribute("estado", estado);
---
+++ drogaria/src/main/java/com/eduardo/v2/drogaria/jpa/ListaEstado.java
@@ -0,0 +1,24 @@
+package com.eduardo.v2.drogaria.jpa;
+
+import com.eduardo.v2.drogaria.domain.Estado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ListaEstado {
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public ListaEstado(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public List<Estado> listar(){
+        CadastraEstado cadastraEstadoestado = applicationContext.getBean(CadastraEstado.class);
+        List<Estado> estados = cadastraEstadoestado.listar();
+        return estados;
+    }
+}
