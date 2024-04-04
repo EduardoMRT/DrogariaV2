7cb840191e5403be3b435f56b62d44da8c3a723c
EduardoMRT <eduksoficial@gmail.com>
2024-04-04 15:42:59 -0300

    updt

--- drogaria/src/main/java/com/eduardo/v2/drogaria/bean/UsuarioBean.java
+++ drogaria/src/main/java/com/eduardo/v2/drogaria/bean/UsuarioBean.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Objects;
 
@@ -33,7 +34,7 @@ public class UsuarioBean {
     }
 
     @PostMapping(value = "drogariaV2/login", params = "action=entrar")
-    public String entrar(Model model, String cpf, String senha){
+    public String entrar(Model model, @RequestParam String cpf, @RequestParam String senha){
         System.out.println("CPF:"+cpf);
 
         Pessoa pessoa = buscaPessoa.buscarPessoaPorCpf(cpf);
